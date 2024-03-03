4aacf4ab7687d56793414dbd695bc68594452db4
Dan Lyu <dan@muddy.ca>
2024-03-02 23:25:00 -0500

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+
+<main id="main-page" class="flex flex-col items-center w-full h-full overflow-auto">
+	<label class="input input-bordered flex items-center gap-2">
+		Name
+		<input type="text" class="grow" placeholder="Daisy" />
+	</label>
+</main>
