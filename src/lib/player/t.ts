c79c5e09cbc5757bbd0e8b5dbec74044e4776cda
Dan Lyu <dan@muddy.ca>
2024-05-15 03:25:36 -0400

    Update formatSeconds

--- src/lib/player/t.ts
+++ src/lib/player/t.ts
@@ -99,12 +99,11 @@ export function formatSeconds(seconds: number | undefined): string {
 	if (seconds === undefined) {
 		return '00:00';
 	}
-	// convert seconds to minutes and seconds
 	const minutes = Math.floor(seconds / 60);
 	const remainingSeconds = Math.floor(seconds % 60);
-	// add leading zero if seconds < 10
 	const secondsStr = remainingSeconds < 10 ? `0${remainingSeconds}` : remainingSeconds;
-	return `${minutes}:${secondsStr}`;
+	const minutesStr = minutes < 10 ? `0${minutes}` : minutes;
+	return `${minutesStr}:${secondsStr}`;
 }
 
 export function randomString(length: number): string {
