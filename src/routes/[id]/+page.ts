cd22ed49db593b269ee603f0d64540c9b4afd0cd
Dan Lyu <dan@muddy.ca>
2024-05-17 01:13:18 -0400

    Update video metadata

--- src/routes/[id]/+page.ts
+++ src/routes/[id]/+page.ts
@@ -5,15 +5,19 @@ import type { Job } from '$lib/player/t';
 export async function load({ params }) {
 	const { id } = params;
 	let job: Job | null = null
+	let video = `${PUBLIC_HOST}/static/` + id + '/av1.mp4'
 	try {
 		const titleResponse = await fetch(`${PUBLIC_HOST}/static/` + id + '/job.json');
 		job = await titleResponse.json();
+		if (!job?.EncodedCodecs?.includes('av1')) {
+			video = `${PUBLIC_HOST}/static/` + id + '/hevc.mp4'
+		}
 	} catch (e) {
 		console.log(params, e);
 	}
 	return {
 		job: job,
-		video: `${PUBLIC_HOST}/static/` + id + '/av1.mp4',
+		video: video,
 		preview: `${PUBLIC_HOST}/static/` + id + '/thumb.jpg',
 	};
 }
