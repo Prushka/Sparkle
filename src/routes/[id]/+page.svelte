dccc1511d4a3b2b4ecdb8319c5574659df036255
Dan Lyu <dan@muddy.ca>
2024-06-02 13:16:28 -0400

    Update tags

--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -9,13 +9,19 @@
 		vidstack = (await import('$lib/player/Player.svelte')).default;
 	});
 
+	$: title = "Watch anime with Hmph! - " + data.title;
 </script>
 
 <svelte:head>
-	<title>{data.title}</title>
-	<meta property="og:title" content={data.title}>
+	<title>{title}</title>
+	<meta name="theme-color" content="#FEA7A6">
+	<meta property="og:title" content={title}>
 	<meta property="og:image" content={data.preview}>
 	<meta property="og:video" content={data.video}>
+	<meta property="og:video:url" content={data.video}>
+	<meta property="og:video:width" content={data.job?.width}>
+	<meta property="og:video:height" content={data.job?.height}>
+	<meta property="og:video:type" content="video/mp4" />
 	<meta property="og:image:type" content="image/jpeg">
 	<meta property="og:description" content={data.plot}>
 	<meta name="description" content={data.plot} />
