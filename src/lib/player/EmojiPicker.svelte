51d2249bf74c3147cbe6b62d5099f3874f284797
Dan Lyu <dan@muddy.ca>
2024-07-07 03:00:49 -0400

    Remove emojis

--- src/lib/player/EmojiPicker.svelte
+++ src/lib/player/EmojiPicker.svelte
@@ -1,12 +1,8 @@
 <script lang="ts">
-	import { smilies } from '$lib/player/emoji';
 	import { browser } from '$app/environment';
 </script>
 
 {#if browser}
 	<div class="flex flex-wrap gap-1">
-		{#each smilies as emoji}
-			<img src={emoji.download_url} alt={emoji.name.slice(-3)} width="20" height="20" />
-		{/each}
 	</div>
 {/if}
