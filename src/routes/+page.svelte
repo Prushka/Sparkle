1d321af984a7d1dac20300ed37ed9e608bac81e2
Dan Lyu <dan@lyu.sh>
2024-07-08 01:56:11 -0400

    Update default title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,11 @@
 	export let data: any;
 </script>
 
+<svelte:head>
+	<title>It's anime time!</title>
+</svelte:head>
+
+
 <div class="p-10 gap-2 w-full flex items-center flex-col justify-center mt-20">
 
 <MediaSelection data={data}/>
